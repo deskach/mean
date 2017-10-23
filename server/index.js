fd684bb1e85af25c857076d38d82582ed376daf6
deskach <dzianis.kachanau@gmail.com>
2017-10-24 10:01:53 +1100

    Hooked mongoose into the app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(keys.mongoURL);
+
 const app = express();
 
 require('./routes/authRoutes')(app);
