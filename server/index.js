6d567981f227393a02333e8c54dafd8120d81a0d
deskach <dzianis.kachanau@gmail.com>
2017-10-24 08:00:40 +1100

    Hooked google oauth via passport

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
+const PORT = process.env.PORT || 5000;
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send({hello: 'world'});
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(accessToken);
+  console.log(refreshToken);
+  console.log(profile);
+}));
 
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 console.log(`Server is running on port ${PORT}`);
 
