799ca22e4d9923877e1062f3665e776accc11fe9
deskach <dzianis.kachanau@gmail.com>
2017-10-24 12:16:16 +1100

    Finished cookie-based authentication

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+
+
+// Init mongo schemas
+require('./models/user');
+
+// Init passport
 require('./services/passport');
 
+// Init express
 const PORT = process.env.PORT || 5000;
-
-mongoose.connect(keys.mongoURL);
-
 const app = express();
 
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  keys: [keys.cookieKey],
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Init routes
 require('./routes/authRoutes')(app);
+
+// Start services
+mongoose.connect(keys.mongoURL);
+app.listen(PORT);
 
 console.log(`Server is running on port ${PORT}`);
 
-app.listen(PORT);
