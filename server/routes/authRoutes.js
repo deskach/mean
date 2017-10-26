7f9af57879d9284cbc910609d2dcf92805a63485
deskach <dzianis.kachanau@gmail.com>
2017-10-26 13:49:51 +1100

    added session info to /api/current_user route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -12,5 +12,8 @@ module.exports = (app) => {
   app.get('/auth/google/callback', passport.authenticate('google'));
 
   app.get('/api/logout', logOut);
-  app.get('/api/current_user', (req, res) => res.send(req.user));
+  app.get('/api/current_user', (req, res) => res.send({
+    session: req.session,
+    user: req.user
+  }));
 };
