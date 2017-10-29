2d11567a4f4ba56be02c7118c6ecec5d8c48ff55
deskach <dzianis.kachanau@gmail.com>
2017-10-30 09:44:30 +1100

    refactor back-end to use async/await instead of promises

--- server/services/passport.js
+++ server/services/passport.js
@@ -18,16 +18,16 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true,
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if (existingUser) {
-        done(null, existingUser);
-      } else {
-        new User({ googleId: profile.id })
-          .save()
-          .then(newUser => done(null, newUser));
-      }
-    });
+}, async (accessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({ googleId: profile.id });
+
+  if (existingUser) {
+    done(null, existingUser);
+  } else {
+    const newUser = new User({ googleId: profile.id });
+
+    await newUser.save();
+    done(null, newUser);
+  }
 }));
 
