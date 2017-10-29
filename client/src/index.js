a3baaa51c052d8620faad653d3ca5efe89f1c6ab
deskach <dzianis.kachanau@gmail.com>
2017-10-30 10:39:26 +1100

    hook reducers

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './components/App';
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
+import reducers from './reducers';
 // import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}><App/></Provider>,
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,6 @@
+export default (state = {}, action = null) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
